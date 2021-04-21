c0a6020946b6bd55d42c19816e8cbadeb90521b5
WebBoroda <webborodaweb@gmail.com>
2021-04-21 12:11:02 +0300

    add btn and JS header fixed

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,21 @@
-/**
-  * название функции
-  *
-  * @param {number} first - первое число
-  * @returns {number}
-  */
+// fixed header
+const headerTop = document.querySelector('.header__top');
+const headerBottom = document.querySelector('.header__bottom');
+const headerTopHeight = headerTop.offsetHeight;
+const headerBottomHeight = headerBottom.offsetHeight;
+
+window.addEventListener('scroll', () => {
+  let scrollDistance = window.scrollY;
+
+  if (scrollDistance >= headerBottomHeight + headerTopHeight) {
+    headerTop.classList.add('header__top--fixed');
+    headerBottom.style.marginTop = `${headerTopHeight}`;
+  } else {
+    headerTop.classList.remove('header__top--fixed');
+    headerBottom.style.marginTop = null;
+  }
+});
+
 
 
 // select
